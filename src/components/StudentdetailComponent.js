fae677d6fc9ece296e8e1fa2bfe2beebae69ce96
Divyashree <60806201+Divz14@users.noreply.github.com>
2020-11-03 19:29:39 +0530

    Update StudentdetailComponent.js

--- src/components/StudentdetailComponent.js
+++ src/components/StudentdetailComponent.js
@@ -1,39 +1,45 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
-import { studentdetail } from '../shared/studentdetail';
 
+import { studentdetail } from '../shared/studentdetail';
+import MA from '../images/mavatar.png/.';
 
 function Studentdetails() {
 	return (
-		<div className="studentdetails">
-		<Table striped bordered hover>
-			<thead>
-				<tr>
-					<th>#</th>
-					<th>Name</th>
-					<th>Email</th>
-					<th>ID</th>
-					<th>Sex</th>
-					<th>School</th>
-					<th>Enrolled Course</th>
-					<th>Enrolled Course</th>
-				</tr>
-			</thead>
-			<tbody>
-					<tr>
-						<td></td>
-						<td>{studentdetail[0].name}</td>
-						<td>{studentdetail[0].email}</td>
-						<td>{studentdetail[0].identity}</td>
-						<td>{studentdetail[0].sex}</td>
-						<td>{studentdetail[0].school}</td>
-						<td>{studentdetail[0].course1}</td>
-						<td>{studentdetail[0].course2}</td>
+		<div className="col-sm studentdetails">
+			<img src={MA} />
+			<br/>
+			<div className="card-group4">
+		<div className="card-group3">
+		<label> Name</label>
+		<input type="text" value={studentdetail[0].name} readonly /><br/>
+		<label >Email</label>
+		<input type="email" value={studentdetail[0].email} readonly /><br/>
+		<label > ID</label>
+		<input type="number" value={studentdetail[0].identity} readonly/><br/>
+		</div>
+		<div calssNmae="card-group3">		
+		<label> Sex</label>
+		<input type="text" value={studentdetail[0].sex} readonly /><br/>
+		<label> School</label>
+		<input type="text" value={studentdetail[0].school} readonly /><br/>
+		<label> Courses
+			<br />
+			{studentdetail[0].course1}
+						
+						{studentdetail[0].course2}</label> <br/>
+		
+	
+		
+	
+					
 
-					</tr>
-			</tbody>
-		</Table>
-	</div>
+
+</div>
+</div>
+
+					
+			
+			</div>
 	);
 }
 
