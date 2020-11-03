5859c8048d2de02ab8aa0fcdcb07b9c1f6ef58e2
Divyashree <60806201+Divz14@users.noreply.github.com>
2020-11-03 19:26:04 +0530

    Update TeamComponent.js

--- src/components/TeamComponent.js
+++ src/components/TeamComponent.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import AJ from '../images/AJ.png/.';
+import DIVE from '../images/Div.png/.';
+import RUN from '../images/Run.png/.';
+import LOOSU from '../images/Loosifer.jpeg/.';
+import './Team.css/.';
 
 function Team() {
 	return (
 		<div className="team">
+			
 			<h1> Our Members</h1>
+			<div class="card-group2">
+
+			<div className="card2">
+    
+    <div className="card-body2">
+	<h4 className="card-text2">AJAY KUMAR</h4>
+    <img className="card-img img1" src={AJ} alt="Card image"  />
+	</div>
+      </div>
+			
+	  <div className="card2">
+    
+    <div className="card-body2">
+	<h4 className="card-text2">ARUN KUMAR</h4>
+    <img className="card-img img1" src={RUN} alt="Card image"  />
+      </div>
+      </div>
+
+	  <div className="card2">
+    
+    <div className="card-body2">
+	<h4 className="card-text2">DIVYA</h4>
+    <img className="card-img img1" src={DIVE} alt="Card image"  />
+ </div>
+      </div>
+
+	  <div className="card2">
+    
+    <div className="card-body2">
+	<h4 className="card-text2">DIVYASHREE</h4>
+    <img className="card-img img1" src={LOOSU} alt="Card image"  />
+     </div>
+      </div>
+		</div>
 		</div>
 	);
 }
